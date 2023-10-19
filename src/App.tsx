6c4bf72375a3b9bba91acad24963d8df03ea4847
Ndungutse Charles <95475354+NdungutseCharles103@users.noreply.github.com>
2023-10-19 16:11:17 +0200

    feat: added eslint-plugin-vitest

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import { useCounter } from './hooks/useCounter';
 import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
    const { count, increment } = useCounter();
 
    return (
-      <div className="App">
-         <header className="flex">
-            <p>Hello Vite + React!</p>
+      <div className=" dark:bg-slate-950 w-full flex flex-col items-center justify-center min-h-screen dark:text-white">
+         <div role="banner" className="flex gap-x-3 items-center">
+            <a href="https://vitejs.dev" target="_blank">
+               <img src={viteLogo} className="w-11" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+               <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+         </div>
+         <h1>Vite + React</h1>
+         <div className="flex p-2  flex-col items-center">
+            <button role="button" className=" border-2 border-blue-700 px-5 py-2 rounded-md" onClick={increment}>
+               count is: {count}
+            </button>
             <p>
-               <button type="button" onClick={increment}>
-                  count is: {count}
-               </button>
+               Edit <code>src/App.tsx</code> and save to test HMR
             </p>
-            <p>
-               Edit <code>App.tsx</code> and save to test HMR updates.
-            </p>
-            <p>
-               <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                  Learn React
-               </a>
-               {' | '}
-               <a className="App-link" href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener noreferrer">
-                  Vite Docs
-               </a>
-            </p>
-         </header>
+         </div>
+         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       </div>
    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
    content: ['./src/**/*.{tsx,ts,js,jsx}', './index.html'],
