4e2cdfd4534e04ec409c791f72048fd7ebd3fca1
Ndungutse Charles <95475354+NdungutseCharles103@users.noreply.github.com>
2023-10-19 16:11:17 +0200

    feat: added eslint-plugin-vitest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,8 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
    content: ['./src/**/*.{tsx,ts,js,jsx}', './index.html'],
